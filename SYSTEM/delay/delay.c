5a8d182482a8675bf692ce115d53c40b19298d7b
Neyzoter <sonechaochao@gmail.com>
2019-04-08 16:51:05 +0800

    Beautifully change : delay.c/h

--- SYSTEM/delay/delay.c
+++ SYSTEM/delay/delay.c
@@ -1,14 +1,25 @@
+/**
+  ******************************************************************************
+  * @file    delay.c
+  * @author  zju.nesc.iotlab    浙江大学NeSC课题组物联网实验室
+  * @version V1.0
+  * @date    8-April-2019
+  * @brief   delay function   延时函数
+  ******************************************************************************
+  */
+
+//-------------include---------------//
 #include "delay.h"
 #include "sys.h"
 
 static u8  fac_us=0;							//us延时倍乘数			   
 static u16 fac_ms=0;							//ms延时倍乘数,在os下,代表每个节拍的ms数
 	
-			   
-//初始化延迟函数
-//当使用OS的时候,此函数会初始化OS的时钟节拍
-//SYSTICK的时钟固定为AHB时钟的1/8
-//SYSCLK:系统时钟频率
+/**
+  * @brief  初始化延迟函数
+  * @param  SYSCLK：系统时钟频率
+  * @retval None
+  */
 void delay_init(u8 SYSCLK)
 {
  	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); 
@@ -16,9 +27,11 @@ void delay_init(u8 SYSCLK)
 	fac_ms=(u16)fac_us*1000;				//非OS下,代表每个ms需要的systick时钟数   
 }								    
 
-//延时nus
-//nus为要延时的us数.	
-//注意:nus的值,不要大于798915us(最大值即2^24/fac_us@fac_us=21)
+/**
+  * @brief  延时nus
+  * @param  nus：要延时的us数.
+  * @retval None
+  */
 void delay_us(u32 nus)
 {		
 	u32 temp;	    	 
@@ -32,12 +45,16 @@ void delay_us(u32 nus)
 	SysTick->CTRL&=~SysTick_CTRL_ENABLE_Msk; //关闭计数器
 	SysTick->VAL =0X00;       				//清空计数器 
 }
-//延时nms
-//注意nms的范围
-//SysTick->LOAD为24位寄存器,所以,最大延时为:
-//nms<=0xffffff*8*1000/SYSCLK
-//SYSCLK单位为Hz,nms单位为ms
-//对168M条件下,nms<=798ms 
+
+/**
+  * @brief  延时nms
+  * @param  nms：要延时的ms数
+  * @note   SysTick->LOAD为24位寄存器,所以,最大延时为:
+						nms<=0xffffff*8*1000/SYSCLK
+						SYSCLK单位为Hz,nms单位为ms
+						对168M条件下,nms<=798ms 
+  * @retval None
+  */
 void delay_xms(u16 nms)
 {	 		  	  
 	u32 temp;		   
@@ -53,10 +70,15 @@ void delay_xms(u16 nms)
 } 
 //延时nms 
 //nms:0~65535
+/**
+  * @brief  延时nms
+  * @param  nms：要延时的ms数
+  * @note   nms取值范围0~65535
+  * @retval None
+  */
 void delay_ms(u16 nms)
 {	 	 
-	u8 repeat=nms/540;						//这里用540,是考虑到某些客户可能超频使用,
-											//比如超频到248M的时候,delay_xms最大只能延时541ms左右了
+	u8 repeat=nms/540;
 	u16 remain=nms%540;
 	while(repeat)
 	{
--- SYSTEM/delay/delay.h
+++ SYSTEM/delay/delay.h
@@ -1,5 +1,17 @@
+/**
+  ******************************************************************************
+  * @file    delay.h
+  * @author  zju.nesc.iotlab    浙江大学NeSC课题组物联网实验室
+  * @version V1.0
+  * @date    8-April-2019
+  * @brief   delay function   延时函数
+  ******************************************************************************
+  */
+
 #ifndef __DELAY_H
-#define __DELAY_H 			   
+#define __DELAY_H 	
+
+//-------------include---------------//
 #include <sys.h>	  
 
 void delay_init(u8 SYSCLK);
