bfbb2ca876c99ebb747fcee8146f005266e155f3
Neyzoter <sonechaochao@gmail.com>
2019-04-25 21:14:51 +0800

    update

--- HARDWARE/WIFI/userwifi.c
+++ HARDWARE/WIFI/userwifi.c
@@ -23,7 +23,7 @@ u8     DATA_AUTO_CHECK_EN = 1;	    									 //是否在中断中自动check数据
 u32    SYSTEMTIME = 0;                                                   //系统时间
 u32    YYMMDD =0;                                                        //年月日
 u8     Time_Sync_Flag = 0;                                               //最近时钟是否同步
-volatile u8 Wifi_Send_EN = 0;                                            //数据采集和发送使能，是CAN和ADC采集的总开关
+volatile u8 Wifi_Send_EN = 1;                                            //数据采集和发送使能，是CAN和ADC采集的总开关
 u8     CAN_Get_EN = CAN1_ENABLE_BIT_SLC|CAN2_ENABLE_BIT_SLC;             //CAN数据发送使能（第0位使能can1，第1位使能can2），默认开启（必须满足Wifi_Send_EN=1，才能采集）
 u8     ADC_Get_EN = 1;                                                   //ADC数据采集使能，默认开启（必须满足Wifi_Send_EN=1，才能采集）
 Queue  adc_queue;                                                        //ADC数据存储
